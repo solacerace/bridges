519d4445ec5294570eace6e1d4c83b86e45333af
DGiri <deepakgiri123@gmail.com>
2018-02-17 23:39:02 +0800

    Added recursive code for edit distance

--- dp/EditDistance.cpp
+++ dp/EditDistance.cpp
@@ -13,6 +13,34 @@ int min(int x, int y, int z)
 {
 	return min(min(x, y), z);
 }
+
+int editDist(string str1, string str2, int m, int n)
+{
+	// If first string is empty, the only option is to
+	// insert all characters of second string into first
+	if (m == 0) return n;
+
+	// If second string is empty, the only option is to
+	// remove all characters of first string
+	if (n == 0) return m;
+
+	// If last characters of two strings are same, nothing
+	// much to do. Ignore last characters and get count for
+	// remaining strings.
+	if (str1[m - 1] == str2[n - 1])
+		return editDist(str1, str2, m - 1, n - 1);
+
+	// If last characters are not same, consider all three
+	// operations on last character of first string, recursively
+	// compute minimum cost for all three operations and take
+	// minimum of three values.
+	return 1 + min(editDist(str1, str2, m, n - 1),    // Insert
+		editDist(str1, str2, m - 1, n),   // Remove
+		editDist(str1, str2, m - 1, n - 1) // Replace
+	);
+}
+
+
 int editDistDP(string str1, string str2, int m, int n)
 {
 	// Create a table to store results of subproblems
@@ -60,6 +88,8 @@ int main(int argc, char* argv[])
 	string str2 = "saturday";
 
 	cout << editDistDP(str1, str2, str1.size(), str2.size());
+
+	cout << "Recursive = "<< editDist(str1, str2, str1.size(), str2.size());
 
 	return 0;
 }
