7774c32fbfb04c883f2988cd188c4a895f69680a
DGiri <deepakgiri123@gmail.com>
2018-02-18 13:04:11 +0800

    Converted tabs to spaces

--- others/ExcelColumn.cpp
+++ others/ExcelColumn.cpp
@@ -12,50 +12,50 @@ using namespace std;
 
 string excelColumn(int x)
 {
-	if (x > 0)
-	{
-		int rem = x%26;
-		
-		char c[2] = {};
-		if (rem == 0)
-		{
-			c[0] = 'Z';
-			x = x/26 - 1;
-		}
-		else
-		{
-			c[0] = 'A' + rem-1;
-			x = x/26;			
-		}
-		
-		return excelColumn(x) + c;
-	}
-	
-	return "";
+    if (x > 0)
+    {
+        int rem = x%26;
+        
+        char c[2] = {};
+        if (rem == 0)
+        {
+            c[0] = 'Z';
+            x = x/26 - 1;
+        }
+        else
+        {
+            c[0] = 'A' + rem-1;
+            x = x/26;			
+        }
+        
+        return excelColumn(x) + c;
+    }
+    
+    return "";
 
 }
 
 
 int main(int argc, char* argv[])
 {
-	vector<int> v = {2,4,6,8,10};
-	for (auto&& i : v)
-	{
-		cout << i << " " ;
-	}
-	cout << endl;
-	
-	
-	cout << excelColumn(26) << endl;
-	cout << excelColumn(51) << endl;
-	cout << excelColumn(52) << endl;
-	cout << excelColumn(80) << endl;
-	cout << excelColumn(676) << endl;
-	cout << excelColumn(702) << endl;
-	cout << excelColumn(705) << endl;
-	
+    vector<int> v = {2,4,6,8,10};
+    for (auto&& i : v)
+    {
+        cout << i << " " ;
+    }
+    cout << endl;
+    
+    
+    cout << excelColumn(26) << endl;
+    cout << excelColumn(51) << endl;
+    cout << excelColumn(52) << endl;
+    cout << excelColumn(80) << endl;
+    cout << excelColumn(676) << endl;
+    cout << excelColumn(702) << endl;
+    cout << excelColumn(705) << endl;
+    
 
-	return 0;
+    return 0;
 }
 
 
--- others/Rectangle.cpp
+++ others/Rectangle.cpp
@@ -4,16 +4,16 @@ using namespace std;
 
 struct Rectangle
 {
-	int x1;
-	int y1;
-	int x2;
-	int y2;	
-	Rectangle(int xa, int ya, int xb, int yb):
-		x1(xa), 
-		y1(ya),
-		x2(xb),
-		y2(yb)
-		{}	
+    int x1;
+    int y1;
+    int x2;
+    int y2;	
+    Rectangle(int xa, int ya, int xb, int yb):
+        x1(xa), 
+        y1(ya),
+        x2(xb),
+        y2(yb)
+        {}	
 };
 
 
@@ -28,15 +28,15 @@ struct Rectangle
 
 int main(int argc, char* argv[])
 {
-	cout << "Hello World" << endl;
-	
-	Rectangle ra(0, 10, 10, 0), rb(5, 5, 15, 0);
-	
-	if (ra.x2 > rb.x1 && ra.y1 > rb.y2 )
-	
-	
-	
-	cout << "Hello World" << endl;
-	
-	return 0;
+    cout << "Hello World" << endl;
+    
+    Rectangle ra(0, 10, 10, 0), rb(5, 5, 15, 0);
+    
+    if (ra.x2 > rb.x1 && ra.y1 > rb.y2 )
+    
+    
+    
+    cout << "Hello World" << endl;
+    
+    return 0;
 }
